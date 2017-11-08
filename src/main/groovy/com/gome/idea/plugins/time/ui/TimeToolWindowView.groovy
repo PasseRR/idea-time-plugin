dc662c6ce03af0bb1a138e3d04e3f691c59d3308
PasseRR <xie__hai@sina.com>
2017-11-08 11:31:12 +0800

    fixed current day bug

--- src/main/groovy/com/gome/idea/plugins/time/ui/TimeToolWindowView.groovy
+++ src/main/groovy/com/gome/idea/plugins/time/ui/TimeToolWindowView.groovy
@@ -641,6 +641,7 @@ class TimeToolWindowView extends IdeaView {
      */
     private def static isCurrentDay(DayVo day) {
         Calendar calendar = Calendar.getInstance()
+        calendar.setTime(new Date())
         calendar.set(Calendar.HOUR_OF_DAY, 0)
         calendar.set(Calendar.MINUTE, 0)
         calendar.set(Calendar.SECOND, 0)
